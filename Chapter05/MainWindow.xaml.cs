f1386e9457801ecba542a8e92f72db2ce516a0ed
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-10-11 18:47:39 +0200

    Chapter 5

--- Chapter05/MainWindow.xaml.cs
+++ Chapter05/MainWindow.xaml.cs
@@ -10,23 +10,27 @@ namespace Chapter05;
 /// </summary>
 public partial class MainWindow : Window
 {
-	private readonly Product _product;
+	private Product _product;
 
 	public MainWindow()
 	{
 		InitializeComponent();
-
-		_product = new("DZ-10", "długopis żelowy", 132, "Katowice 1");
-		GridProduct.DataContext = _product;
-
-		var colors = typeof(Brushes).GetProperties().Select(x => x.Name);
-		CbColors.ItemsSource = colors;
-		CbColors.SelectedValue = "Red";
+		PrepareBinding();
+		FillColors();
 	}
 
 	private void BtnConfirm_Click(object sender, RoutedEventArgs e)
+		=> MessageBox.Show($"Wprowadzono dane:{Environment.NewLine}{_product}");
+
+	private void PrepareBinding()
 	{
-		string text = $"Wprowadzono dane:{Environment.NewLine}{_product}";
-		MessageBox.Show(text);
+		_product = new("DZ-10", "długopis żelowy", 132, "Katowice 1");
+		GridProduct.DataContext = _product;
+	}
+
+	private void FillColors()
+	{
+		CbColors.ItemsSource = typeof(Brushes).GetProperties().Select(x => x.Name);
+		CbColors.SelectedValue = "Red";
 	}
 }
